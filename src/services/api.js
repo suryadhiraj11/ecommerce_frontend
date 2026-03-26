cace6f1b5c45df5f73929324a6bf6a6861e3c6fb
ACHANTA SURYA DHIRAJ <2400032511@kluniversity.in>
2026-03-26 10:47:46 +0530

    Update API base URL for the service

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ecommerce-backend-production-222b.up.railway.app/api"
+  baseURL:"ecommercebackend-production-a186.up.railway.app/api"
 });
 
 export default API;
