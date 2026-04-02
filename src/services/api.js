20e1df7733421e4bd23f335aac26ac0d1a4c3b46
ACHANTA SURYA DHIRAJ <2400032511@kluniversity.in>
2026-04-02 09:17:06 +0530

    Update API base URL to use HTTPS

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:"ecommercebackend-production-a186.up.railway.app/api"
+  baseURL:"https://ecommercebackend-production-a186.up.railway.app/api"
 });
 
 export default API;
